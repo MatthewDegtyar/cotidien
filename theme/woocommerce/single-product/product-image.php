aa2c87235a3b187a524fa5faf879a6d7fb093ff7
shz24 <shz24@cornell.edu>
2025-05-03 14:14:15 -0400

    Fix slider buttons

--- theme/woocommerce/single-product/product-image.php
+++ theme/woocommerce/single-product/product-image.php
@@ -51,8 +51,8 @@ $wrapper_classes   = apply_filters(
 
 				<!-- Slider Controls -->
 				<div class="slider-controls">
-					<button id="prev-slide" class="slider-button"><svg class="-rotate-90 scale-200" data-name="1-Arrow Up" xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 32 32"><path d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z"/></svg></button>
-					<button id="next-slide" class="slider-button"><svg class="rotate-90 scale-200" data-name="1-Arrow Up" xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 32 32"><path d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z"/></svg></button>
+					<button id="prev-slide" class="slider-button"><svg class="-rotate-90 scale-200" data-name="1-Arrow Up" xmlns="http://www.w3.org/2000/svg" fill="#000" viewBox="0 0 32 32"><path d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z"/></svg></button>
+					<button id="next-slide" class="slider-button"><svg class="rotate-90 scale-200" data-name="1-Arrow Up" xmlns="http://www.w3.org/2000/svg" fill="#000" viewBox="0 0 32 32"><path d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z"/></svg></button>
 				</div>
 			</div>
 
