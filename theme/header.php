0d72752afdbb3b1ef8573467885940e63cc5f918
shz24 <shz24@cornell.edu>
2025-05-03 23:44:14 -0400

    Change cart from text to svg

--- theme/header.php
+++ theme/header.php
@@ -120,18 +120,34 @@
 			</div>
 
 			<div class="flex flex-row items-center gap-8">
-	
+
+
 			<?php if ( ! is_cart() ) : ?>
-				<button id="cart-toggle" class="relative flex cursor-pointer">
-					<span>Cart</span>
+				<button id="cart-toggle" class="relative flex items-center cursor-pointer">
+					<!-- Inline SVG icon -->
+					<svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+						viewBox="0 0 902.86 902.86" xml:space="preserve" class="w-6 h-6">
+						<g>
+							<path d="M671.504,577.829l110.485-432.609H902.86v-68H729.174L703.128,179.2L0,178.697l74.753,399.129h596.751V577.829z M685.766,247.188
+								l-67.077,262.64H131.199L81.928,246.756L685.766,247.188z"/>
+							<path d="M578.418,825.641c59.961,0,108.743-48.783,108.743-108.744s-48.782-108.742-108.743-108.742H168.717
+								c-59.961,0-108.744,48.781-108.744,108.742s48.782,108.744,108.744,108.744c59.962,0,108.743-48.783,108.743-108.744
+								c0-14.4-2.821-28.152-7.927-40.742h208.069c-5.107,12.59-7.928,26.342-7.928,40.742
+								C469.675,776.858,518.457,825.641,578.418,825.641z M209.46,716.897c0,22.467-18.277,40.744-40.743,40.744
+								c-22.466,0-40.744-18.277-40.744-40.744c0-22.465,18.277-40.742,40.744-40.742C191.183,676.155,209.46,694.432,209.46,716.897z
+								M619.162,716.897c0,22.467-18.277,40.744-40.743,40.744s-40.743-18.277-40.743-40.744c0-22.465,18.277-40.742,40.743-40.742
+								S619.162,694.432,619.162,716.897z"/>
+						</g>
+					</svg>
 
 					<?php if ( function_exists( 'WC' ) && WC()->cart->get_cart_contents_count() > 0 ) : ?>
-						<span class="absolute -top-2 -left-2 bg-red-500 text-white text-[10px] leading-none font-medium px-[6px] py-[2px] rounded-full">
+						<span class="absolute -top-2 -left-2 bg-black text-white text-[10px] leading-none font-medium px-[6px] py-[2px] rounded-full">
 							<?php echo WC()->cart->get_cart_contents_count(); ?>
 						</span>
 					<?php endif; ?>
 				</button>
 			<?php endif; ?>
+
 
 				<button id="menu-toggle" class="lg:hidden flex cursor-pointer">
 					<svg class="w-8 h-8 mt-[8px] text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
