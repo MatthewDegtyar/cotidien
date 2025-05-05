14272d88043743e405f3ac605e3515989f7da157
shz24 <shz24@cornell.edu>
2025-05-04 22:43:00 -0400

    Fix shading on text and home page image

--- theme/index.php
+++ theme/index.php
@@ -43,13 +43,15 @@ $bottom_header = get_theme_mod('home_header_img', $default_img);
 				</div>
 
 				<!-- Gradient: sits above the image -->
-				<div class="absolute top-0 left-0 w-full h-full bg-black/40 lg:bg-gradient-to-r lg:from-black/40 lg:via-black/10 lg:to-black/0 z-20 pointer-events-none"></div>
+				<!-- <div class="absolute top-0 left-0 w-full h-full bg-black/40 lg:bg-gradient-to-r lg:from-black/40 lg:via-black/10 lg:to-black/0 z-20 pointer-events-none"></div> -->
 
 				<!-- Content: defines the natural size of the div -->
 				<div class="main-content-width my-[50px] z-30 text-white max-w-[650px] relative">
-					<h2 class="text-[28px] lg:text-[36px] font-cormorant">COTIDIEN (koh-tee-dee-ahn)</h2>
-					<p>"Cote" (꽃), meaning flower in Korean, and "quotidien," meaning everyday in French, together symbolize our brand's mission: to create beautiful, elevated pieces that seamlessly fit into your everyday wardrobe.</p>
-
+					<h2 class="text-[28px] lg:text-[36px] font-cormorant" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8);">COTIDIEN</h2>
+					<p class="text-[20px] lg:text-[20px] font-cormorant" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8);">(koh-tee-dee-ahn)</p>
+					<br>
+					<p class="text-[16px] lg:text-[16px]" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8);">"Cote" (꽃), the Korean word for flower, and "quotidien," the French word for everyday, reflect our mission: to create beautiful, elevated pieces that seamlessly fit into your wardrobe.</p>
+				
 					<!-- <h3 class="text-[24px] font-cormorant mt-8">Our Story</h3>
 					<p>Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes.</p>
 
@@ -57,8 +59,8 @@ $bottom_header = get_theme_mod('home_header_img', $default_img);
 					<p>Body text for whatever you’d like to add more to the main point. It provides details, explanations, and context.</p> -->
 
 					<div class="mt-8">
-					<a href="/shop" class="bg-white text-[24px] text-black px-8 py-2 hover:brightness-75">
-						Discover Cotidien
+					<a href="/shop" class="font-cormorant bg-white text-[24px] text-black px-8 py-2 hover:brightness-75">
+						View Collection I
 					</a>
 					</div>
 				</div>
--- theme/page-about.php
+++ theme/page-about.php
@@ -21,7 +21,8 @@ $img3 = get_theme_mod('about_img_3', $default_img);
 
 <main class="threads-page">
     <div class="relative">
-        <h1 class="text-3xl lg:text-[58px] text-white font-cormorant absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:text-nowrap text-center z-10">
+        <h1 class="text-3xl lg:text-[58px] text-white font-cormorant absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:text-nowrap text-center z-10"
+        style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">
         New here? We’re happy you found us.    
         </h1>
 
@@ -33,10 +34,14 @@ $img3 = get_theme_mod('about_img_3', $default_img);
         <div class="flex flex-col md:flex-row relative mt-[55px]">
             <div class="md:w-[50%]">
                 <h2 class="text-[24px]">February 2025</h2>
-                <p class="mt-[40px] text-[#828282]" style="margin-right: 20px;">Cotidien started from that restless need to create, an energy that couldn't wait for permission. It began with flowers, then fabric, chasing that same feeling: freedom, clarity, and the kind of joy that shows up when you're fully in it. That creative pulse is what runs through every piece.</p>
-                <br>
+                <p class="mt-[40px] text-[#828282]" style="margin-right: 20px;">Hi, I'm Sophia, the girl behind Cotidien. Cotidien first came into consciousness via my old sewing machine during 2020 as a way to pass time between online classes. I fell in love with flipping, crafting, and creating pieces that felt true to my style and vision, where I could really sink into the details of each piece and add easter eggs that only I would know to find. At the time, I couldn’t find clothes I loved on mainstream sources, so I made them myself.</p>
+                <!-- <br> -->
                 <p class="mt-[40px] text-[#828282]" style="margin-right: 20px;">
-                In February 2025, it all came together. Cotidien became real — a space to build a wardrobe that felt elevated but easy, thoughtful but never fussy. Made for the everyday and memorable moments in between. </p>
+                In 2024, I started posting the process on Instagram. Over time, a little community formed of individuals who bought into the journey of creating something from the ground up.</p>
+                <!-- <br> -->
+                <p class="mt-[40px] text-[#828282]" style="margin-right: 20px;">
+                In 2025, the sewing journey crystalized into something more tangible through Cotidien. "Cote" (꽃), meaning flower in Korean, and "quotidien," meaning everyday in French, together symbolize our brand's mission: to create beautiful, elevated pieces that seamlessly fit into your everyday wardrobe.
+                </p>
             </div>
             <div class="md:w-[50%] mt-8 md:mt-0">
                 <!-- Image 1 (Using Custom or Default Image) -->
